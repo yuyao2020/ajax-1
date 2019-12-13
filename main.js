7f0fd99fe2cfe700ac6e975b2ef04a7b17254af6
yuyao <980903093@qq.com>
2019-12-13 10:57:45 +0800

    ajax封装完成

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
-myButton.addEventListener('click', (e)=>{
+window.jQuery = function (nodeOrSelector) {
+  let nodes = {}
+  nodes.addClass = function () {}
+  nodes.html = function () {}
+  return nodes
+}
+
+window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) {
+  /*
+  if(arguments.length === 1){
+    url = options.url
+  }else if(arguments.length === 2){
+    url = arguments[0]
+    options = arguments[1]
+  }
+  两种参数的写法*/
+
   let request = new XMLHttpRequest()
-  request.open('GET', 'http://localhost:8888/xxx')//配置request
-  //request.onreadystatechange
-  request.send()
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      console.log("请求响应都完毕了")
-    
-      if(request.status >= 200 && request.status < 300){
-        console.log("说明请求成功")
-        console.log(request.responseText)
-        let string = request.responseText
-        let object = window.JSON.parse(string)//把符合json语法的字符串转换成js对应的值
-        console.log(object)
-        console.log('object.note.to')
-        console.log(object.note.to)
-      }else if(request.status >= 400){
-        console.log("说明请求失败")
+  request.open(method, url) //配置request
+  for(let key in headers){
+    let value = headers[key]
+    request.setRequestHeader(key, value) 
+  }//添加header
+
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        successFn.call(undefined, request.responseText)
+      } else if (request.status >= 400) {
+        failFn.call(undefined, request)
       }
     }
   }
-  
+  request.send(body)
+}
+window.$ = window.jQuery
+
+
+myButton.addEventListener('click', (e) => {
+  window.jQuery.ajax({
+    url: '/xxx',
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'yuyao': '18'
+    },
+    body: 'a=111&b222',
+    successFn: (x) => {
+      console.log(x)
+    },
+    failFn: (x) => {
+      console.log(x.status)
+      console.log(x.responseText)
+    }//用对象的形式将参数们包裹起来)
+})
 })
